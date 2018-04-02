7ce4399bf0e703816e0c1c878caf341bd4b270db
Your Name <you@example.com>
2018-04-02 10:44:43 +0800

    添加quartz定时任务配置

---
+++ dangyuan-web/src/main/java/com/telecom/jx/dangyuan/quartz/MyQuartz.java
@@ -0,0 +1,12 @@
+package com.telecom.jx.dangyuan.quartz;
+
+public class MyQuartz {
+
+    public void test() {
+        System.out.println("MyQuartz");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
